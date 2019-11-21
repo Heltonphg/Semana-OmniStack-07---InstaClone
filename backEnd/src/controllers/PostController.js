f3744939020509ec458fd74742ab75da2cecfb9a
wylammi.helton <wylammi.helton@gmail.com>
2019-11-21 18:00:53 -0200

    tratando imagens de forma eficaz

--- backEnd/src/controllers/PostController.js
+++ backEnd/src/controllers/PostController.js
@@ -1,21 +1,36 @@
 const Post = require('../models/Post');
+const sharp = require('sharp');//? lidar com redimensionamento de imagens
+const path = require('path');//? lidar com caminhos
+const fs = require('fs'); //? file systen
 
 class PostController {
 
-    async index(req,res) {
-        const posts = await Post.find();
-        return res.send(posts);
+    async index(req, res) {
+        const posts = await Post.find().sort('-createdAt');
+        return res.json(posts);
     }
 
-    async store(req,res) {
-        const {author, place, description, hashtags} = req.body;
-        const {filename: image} = req.file;
+    async store(req, res) {
+        const { author, place, description, hashtags } = req.body;
+        const { filename: image } = req.file;
+
+        //TODO: Redimensionando imagem -> 
+        //TODO: dps que a foto foi salva pelo arquivo upload.js, o sharp redimensiona e a coloca em resized
+        await sharp(req.file.path)
+            .resize(500)
+            .jpeg({ quelity: 70 })
+            .toFile(
+                path.resolve(req.file.destination, 'resized', image)
+            );
+        fs.unlinkSync(req.file.path); //TODO: Apagando a imagem do caminho inicial
+
+        //TODO: persistindo dados no mongodb
         const post = await Post.create({
             author,
             place,
             description,
             hashtags,
-            image 
+            image
         })
         return res.json(post);
     }
--- backEnd/src/routes.js
+++ backEnd/src/routes.js
@@ -1,12 +1,15 @@
 const express = require ('express')
 const PostController = require('./controllers/PostController');
+const LikeController = require('./controllers/LikeController');
 const multer = require('multer');
 const uploadConfig = require('./config/upload');
+
 
 const router = express.Router();
 const upload = multer(uploadConfig);
 
 router.post('/posts', upload.single('image'), PostController.store);
-router.get('/posts', upload.single('image'), PostController.index);
+router.get('/posts', PostController.index);
+router.post('/posts/:id/like', LikeController.store);
 
 module.exports = router;
