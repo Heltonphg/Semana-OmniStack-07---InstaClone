c6b03bc11229cc82fb11cd7603617fc29d82cddd
wylammi.helton <wylammi.helton@gmail.com>
2019-11-21 12:39:37 -0200

    feature (index and store)  in Posts

--- backEnd/src/controllers/PostController.js
+++ backEnd/src/controllers/PostController.js
@@ -3,11 +3,21 @@ const Post = require('../models/Post');
 class PostController {
 
     async index(req,res) {
-
+        const posts = await Post.find();
+        return res.send(posts);
     }
 
     async store(req,res) {
-        return res.json({ok: true})
+        const {author, place, description, hashtags} = req.body;
+        const {filename: image} = req.file;
+        const post = await Post.create({
+            author,
+            place,
+            description,
+            hashtags,
+            image 
+        })
+        return res.json(post);
     }
 }
 
--- backEnd/src/routes.js
+++ backEnd/src/routes.js
@@ -1,8 +1,12 @@
 const express = require ('express')
 const PostController = require('./controllers/PostController');
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
 
-const router = express.Router()
+const router = express.Router();
+const upload = multer(uploadConfig);
 
-router.post('/posts', PostController.store);
+router.post('/posts', upload.single('image'), PostController.store);
+router.get('/posts', upload.single('image'), PostController.index);
 
 module.exports = router;
