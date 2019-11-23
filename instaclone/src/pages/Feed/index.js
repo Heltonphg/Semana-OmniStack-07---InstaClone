07adfbacdd87f52b38950e245592f71d4e2b32ef
wylammi.helton <wylammi.helton@gmail.com>
2019-11-23 18:28:58 -0200

    adicionando o refreshing

--- instaclone/src/pages/Feed/index.js
+++ instaclone/src/pages/Feed/index.js
@@ -1,27 +1,41 @@
-import React, {useEffect, useState} from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, FlatList, Text } from 'react-native';
 import api from '../../services/api';
+import FeedItems from './FeedItems';
 
 import Header from '../components/Header'
 
 export default function Feed(props) {
-  const  [feed, setFeed] = useState([]);
+  const [feed, setFeed] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
 
-  async function populationFeed(){
+  async function populationFeed() {
+    setRefreshing(true)
     const response = await api.get('posts');
     setFeed(response.data);
+    setRefreshing(false)
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     //registerToSocket();
     populationFeed();
-  },[])
+  }, [])
 
 
   return (
     <View style={{ flex: 1, backgroundColor: '#F8F8FA' }}>
-     <Header navigation = {props.navigation} />
+      <Header navigation={props.navigation} />
 
-   </View>
+      <FlatList
+        onRefresh={populationFeed}
+        refreshing={refreshing}
+        data={feed}
+        keyExtractor={post => String(post._id)}
+        renderItem={({ item }) => (
+          <FeedItems item={item} />
+        )}
+      />
+
+    </View>
   );
 }
